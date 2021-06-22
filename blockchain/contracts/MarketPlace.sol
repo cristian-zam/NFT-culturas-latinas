f5963d371b99d275ddb2eacfe745a66e7584c856
Cristian Zambrano <cristian.zambrano@cloudmex.io>
2021-06-22 12:12:46 -0600

    Se incluyen los metodos de transferir y quemar tokens NFT dentro del smart contract

--- blockchain/contracts/MarketPlace.sol
+++ blockchain/contracts/MarketPlace.sol
@@ -36,4 +36,36 @@ contract MarketPlace is ERC721URIStorage {
 
         return newItemId;
     }
+
+    /**
+    * Función para transferir un token NFT del propietario a otro address
+    @param to representa a quien vamos a transferir nuestro NFT
+    @param tokenId este valor indica el id que vamos a transferir 
+    @return {addresss}  esta función nos retorna el address del nuevo propietario del NFT
+     */
+
+    function transferArt(address to, uint256 tokenId) public returns (address) {
+        require(_exists(tokenId), "El token que intentas transferir no existe");
+        require(msg.sender == ownerOf(tokenId), "Este token no te pertenece");
+
+        //transferir el token NFT si el token id existe y es nuestro
+        safeTransferFrom(msg.sender, to, tokenId);
+        //retornar el nuevo owner del token NFT
+        return to;
+    }
+
+    /** 
+    * Función para eliminar un token NFT en tu address
+    @param tokenID representa el id del token NFT a eliminar
+    @return {success} si el proceso fue llevado con exito la funcion retorna un true
+     */
+    function burnArt(uint256 tokenId) public returns (bool success) {
+        require(_exists(tokenId), "El token que intentas quemar no existe");
+        require(msg.sender == ownerOf(tokenId), "Este token no te pertenece");
+
+        //Eliminamos el token NFT si el token id existe y es nuestro
+        _burn(tokenId);
+
+        return true;
+    }
 }
--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,37 +1,24 @@
 /**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
+ * Archivo para la configuración del proyecto de truffle.
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// Importación del manejador de wallets
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+//Obtención del mnemonico correspondiente a la seed phrase de nuestra wallet
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+//Validación para permitir solo frases de 12 palabras
+if (!mnemonic || mnemonic.split(" ").length !== 12) {
+  throw new Error("No se pudo encontrar un mnemonico aceptable en el archivo .secret")
+}
 
 module.exports = {
   /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
+   * Las redes defininen como conectarnos al cliente de aurora.
+   * Ejemplo: 
    * $ truffle test --network <network-name>
    */
 
@@ -72,6 +59,14 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+
+    // Se incluye la red de aurora en la configuración de truffle
+    aurora: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://testnet.aurora.dev',),
+      network_id: 0x4e454153,
+      gas: 10000000,
+      from: '0xFf89239B9f5BBe3dD801794712CE55751B2484F1' // CHANGE THIS ADDRESS
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -82,7 +77,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-       version: "0.8.1",   // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.1",   // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
