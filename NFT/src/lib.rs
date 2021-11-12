81759be358c66a3673eb51018c7cf489bfcb393e
dokxo96 <dokxo96@gmail.com>
2021-11-12 14:11:39 -0700

    ed

---
+++ NFT/src/lib.rs
@@ -0,0 +1,182 @@
+
+use near_contract_standards::non_fungible_token::metadata::{
+    NFTContractMetadata, NonFungibleTokenMetadataProvider, TokenMetadata, NFT_METADATA_SPEC,
+};
+use near_contract_standards::non_fungible_token::{Token, TokenId};
+use near_contract_standards::non_fungible_token::NonFungibleToken;
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::collections::LazyOption;
+use near_sdk::json_types::{ValidAccountId,Base64VecU8};
+use near_sdk::{
+    env, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise, PromiseOrValue,
+};
+ 
+
+near_sdk::setup_alloc!();
+/// Balance is type for storing amounts of tokens.
+pub type Balance = u128;
+#[derive(BorshDeserialize, BorshSerialize )]
+pub struct OldContract {
+    tokens: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
+    n_token_on_sale: u64,
+}
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize )]
+pub struct Contract {
+    tokens: NonFungibleToken,
+    metadata: LazyOption<NFTContractMetadata>,
+    n_token_on_sale: u64,
+}
+impl Default for Contract {
+    
+    fn default( ) -> Self {
+      
+     let meta =NFTContractMetadata {
+         spec: NFT_METADATA_SPEC.to_string(),
+         name: "Nativo NFT".to_string(),
+         symbol: "NTV".to_string(),
+         icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+         base_uri: None,
+         reference: None,
+         reference_hash: None,
+     };
+     Self {
+     
+         tokens:NonFungibleToken::new(
+             StorageKey::NonFungibleToken,
+             env::signer_account_id().try_into().unwrap(),
+             Some(StorageKey::TokenMetadata),
+             Some(StorageKey::Enumeration),
+             Some(StorageKey::Approval),
+         ) ,
+         metadata: LazyOption::new(StorageKey::Metadata, Some(&meta)),
+         n_token_on_sale:0 ,
+         
+     }   }
+}
+ 
+const DATA_IMAGE_SVG_NEAR_ICON: &str = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 288 288'%3E%3Cg id='l' data-name='l'%3E%3Cpath d='M187.58,79.81l-30.1,44.69a3.2,3.2,0,0,0,4.75,4.2L191.86,103a1.2,1.2,0,0,1,2,.91v80.46a1.2,1.2,0,0,1-2.12.77L102.18,77.93A15.35,15.35,0,0,0,90.47,72.5H87.34A15.34,15.34,0,0,0,72,87.84V201.16A15.34,15.34,0,0,0,87.34,216.5h0a15.35,15.35,0,0,0,13.08-7.31l30.1-44.69a3.2,3.2,0,0,0-4.75-4.2L96.14,186a1.2,1.2,0,0,1-2-.91V104.61a1.2,1.2,0,0,1,2.12-.77l89.55,107.23a15.35,15.35,0,0,0,11.71,5.43h3.13A15.34,15.34,0,0,0,216,201.16V87.84A15.34,15.34,0,0,0,200.66,72.5h0A15.35,15.35,0,0,0,187.58,79.81Z'/%3E%3C/g%3E%3C/svg%3E";
+
+#[derive(BorshSerialize, BorshStorageKey)]
+enum StorageKey {
+    NonFungibleToken,
+    Metadata,
+    TokenMetadata,
+    Enumeration,
+    Approval,
+}
+
+#[near_bindgen]
+impl Contract {
+    /// Initializes the contract owned by `owner_id` with
+    /// default metadata (for example purposes only).
+    // Esta función incializa el contrato con los valores especificados en la metadata
+
+    #[init]
+    pub fn new_default_meta(owner_id: ValidAccountId) -> Self {
+        Self::new(
+            owner_id,
+            NFTContractMetadata {
+                spec: NFT_METADATA_SPEC.to_string(),
+                name: "Nativo NFT".to_string(),
+                symbol: "NTV".to_string(),
+                icon: Some(DATA_IMAGE_SVG_NEAR_ICON.to_string()),
+                base_uri: None,
+                reference: None,
+                reference_hash: None,
+            },
+        )
+    }
+
+    #[init]
+    pub fn new(owner_id: ValidAccountId, metadata: NFTContractMetadata) -> Self {
+        assert!(!env::state_exists(), "Already initialized");
+        metadata.assert_valid();
+        Self {
+            tokens: NonFungibleToken::new(
+                StorageKey::NonFungibleToken,
+                owner_id,
+                Some(StorageKey::TokenMetadata),
+                Some(StorageKey::Enumeration),
+                Some(StorageKey::Approval),
+            ),
+            metadata: LazyOption::new(StorageKey::Metadata, Some(&metadata)),
+            n_token_on_sale: 0,
+        }
+    }
+
+    fn enum_get_token(&self, owner_id: AccountId, token_id: TokenId) -> Token {
+        let metadata = self
+            .tokens
+            .token_metadata_by_id
+            .as_ref()
+            .unwrap()
+            .get(&token_id);
+        let approved_account_ids = Some(
+            self.tokens
+                .approvals_by_id
+                .as_ref()
+                .unwrap()
+                .get(&token_id)
+                .unwrap_or_default(),
+        );
+
+        Token {
+            token_id,
+            owner_id,
+            metadata,
+            approved_account_ids,
+        }
+    }
+
+
+    //all methods created 
+      /**
+     * permite minar nuevos tokens
+     * @param token_owner_id {ValidAccountId} a quien le va a pertenecer el token
+     * @param tokenMetadata {TokenMetadata} los metadatos
+     */
+    #[payable]
+    pub fn minar(&mut self,token_owner_id: ValidAccountId,token_metadata: TokenMetadata,) -> Token {
+        self.n_token_on_sale += 1;
+        self.tokens.mint(
+            self.tokens.owner_by_id.len().to_string(),
+            token_owner_id,
+            Some(token_metadata),
+        )
+    }
+
+    pub fn storage_byte_cost() -> Balance {
+        env::storage_byte_cost()
+    }
+ 
+ 
+    #[private]
+    #[init(ignore_state)]
+    pub fn migrate() -> Self {
+        let old_state: OldContract = env::state_read().expect("failed");
+        Self {
+           
+            tokens:old_state.tokens,
+            metadata: old_state.metadata,
+            n_token_on_sale: old_state.n_token_on_sale,
+        }
+    }
+   
+
+
+}
+
+near_contract_standards::impl_non_fungible_token_core!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_approval!(Contract, tokens);
+near_contract_standards::impl_non_fungible_token_enumeration!(Contract, tokens);
+
+#[near_bindgen]
+impl NonFungibleTokenMetadataProvider for Contract {
+    fn nft_metadata(&self) -> NFTContractMetadata {
+        self.metadata.get().unwrap()
+    }
+}
+
+
